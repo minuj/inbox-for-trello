c6ba3a50e96ecf0c6ea733d5f01eaab609c8f493
Charles Halliday <realchalliday@gmail.com>
2013-08-03 17:03:29 -0700

    added google analytics

--- popup.js
+++ popup.js
@@ -51,3 +51,15 @@ chrome.tabs.getSelected(null, function(tab) {
     // $('#url').val(tab.url);
     // $('#title').val(tab.title);
 });
+
+//google analytics of course
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-24359873-10']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
